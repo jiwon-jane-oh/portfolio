2ab046c1c7d3f706fb5a93f2f6c97f1da942371d
jiwon-jane-oh <cpfldk93@gmail.com>
2024-07-15 16:31:11 -0700

    Observer-part2

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,39 @@
 'use strict';
+
+//projects filtering logic
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+
+  if (filter == null) {
+    return;
+  }
+
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  //reset the active menu
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  //project filtering
+  projects.forEach((project) => {
+    if (filter == 'all' || project.dataset.type === filter) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+}
